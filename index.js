4f9c36ff368f52d378db5f2e6deb486ffae577e9
Carrie Miskin <clove40@gmail.com>
2022-03-15 13:52:08 -0400

    commit tree 692b8337e9fdf3237be05c63379dd8e06a07682a

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution in this file!
+// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']; 
+
+// function returnFirstTwoDrivers() {
+//     return firstTwo;
+// }
+
+// function returnLastTwoDrivers() {
+
+//     return lastTwo;
+// }
+
+// let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// function createFareMultiplier(x) {
+//     return function (){
+//         return x * x 
+//     }
+// }
+
+// let fareDoubler = x => x * 2
+// let fareTripler = x => x * 3
+
+// function selectDifferentDrivers(selectDifferentDrivers, driver) {
+//     if (selectDifferentDrivers = driver, returnFirstTwoDrivers)
+//     return firstTwo;
+//     if (selectDifferentDrivers = driver, returnLastTwoDrivers)
+//     return lastTwo;
+// }
+
+// solution
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+  };
